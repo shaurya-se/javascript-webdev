5ecceb83f55b0b57492cde3e92064fe7bfdb7609
shaurya-se <123918958+shaurya-se@users.noreply.github.com>
2024-08-11 11:32:03 +0530

    conversion in javascript

---
+++ 01_basics/conversions.js
@@ -0,0 +1,10 @@
+let somenumber=22
+let somestring=String(somenumber)
+console.log(somestring)
+console.log(typeof somestring)
+let boolnumber=Boolean(somenumber)
+console.log(boolnumber);
+let somestrings="330q"
+let somenum=Number(somestrings)
+console.log(somenum);
+console.log(typeof somenum);
