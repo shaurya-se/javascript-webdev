5b2c99f64c218946d0573d4b37634a0dc712d59a
shaurya-se <123918958+shaurya-se@users.noreply.github.com>
2024-08-11 11:25:10 +0530

    variables in javascript

---
+++ 01_basics/variable..js
@@ -0,0 +1,6 @@
+const accountId=12345
+let accountname="shaurya"
+var accountEmail="shauryakeshribxr2004@gmail.com"
+// accountCity="buxar"
+console.log(accountname)
+console.table([accountname,accountEmail,accountId])
