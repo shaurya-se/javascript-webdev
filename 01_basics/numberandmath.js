2aab56deb5e12b45d1373e52e09d45afd7eb98e0
shaurya-se <123918958+shaurya-se@users.noreply.github.com>
2024-08-12 22:00:52 +0530

    number and maths

---
+++ 01_basics/numberandmath.js
@@ -0,0 +1,30 @@
+// const balance=300
+// console.log(balance);
+// const newnumber=new Number(300)
+// console.log(newnumber);
+// console.log(newnumber.toString());
+// console.log(newnumber.toFixed(2));
+// const anothernumber=123.245
+// console.log(anothernumber.toPrecision(4));
+// console.log(anothernumber.toPrecision(2));
+// console.log(anothernumber.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++Maths++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.max(1,2,4,7,9));
+// console.log(Math.min(1,2,4,7,9));
+// console.log(Math.round(5.6));
+// console.log(Math.round(5.2));
+// console.log(Math.ceil(5.6));
+// console.log(Math.floor(5.6));
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+const formula=Math.floor(Math.random()*(max-min+1))+min ///very very important formula memorize itttttt
+console.log(formula);
