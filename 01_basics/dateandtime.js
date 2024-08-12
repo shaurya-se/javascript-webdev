f94754b659c7fc1c0d4efe928950cb4b0a9a633f
shaurya-se <123918958+shaurya-se@users.noreply.github.com>
2024-08-12 22:00:15 +0530

    strings

---
+++ 01_basics/dateandtime.js
@@ -0,0 +1,19 @@
+// let mydate=new Date();
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+//  let newdate=new Date(2003,10,14)
+//  console.log(newdate.toDateString());
+//  let newdate=new Date(2003,10,14,5,7)
+//  console.log(newdate.toString());
+//  let newdate=new Date("01-14-2023")
+// console.log(newdate.toDateString());
+
+let mytimestamp=Date.now()
+console.log(mytimestamp);
+console.log(mytimestamp/1000)
+// console.log(newdate.getMonth());
+
+let now=new Date()
+console.log(now.getMilliseconds());
