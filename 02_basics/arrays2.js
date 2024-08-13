9e69f3f85276713daf8272b689f93cd33e920bb6
shaurya-se <123918958+shaurya-se@users.noreply.github.com>
2024-08-13 23:44:44 +0530

    arrays 2

---
+++ 02_basics/arrays2.js
@@ -0,0 +1,19 @@
+let arr1=["shaurya"]
+let arr2=["keshri"]
+// arr1.push(arr2)
+// console.log(arr1);
+// let arr=arr1.concat(arr2)
+// console.log(arr);
+// let spreadarr=[...arr1,...arr2]
+// console.log(spreadarr);
+// let another_arr=[1,2,3,[4,5,[6,7]]]
+// console.log(another_arr.flat(Infinity));
+// console.log(Array.isArray("shaurya"));
+// console.log(Array.from("shaurya"));
+// console.log(Array.isArray("shaurya"));
+console.log(Array.from({name:"shaurya"}));// this is an interesting case as we have to provide on which basis we have to form the array either on key or either on values
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));
