3e33f3f534d99cebeb55d632d37588eff99b7513
shaurya-se <123918958+shaurya-se@users.noreply.github.com>
2024-08-13 18:13:14 +0000

    arrays-1

---
+++ 02_basics/arrays1.js
@@ -0,0 +1,33 @@
+// let myarr=[1,2,3,4,5,"shaurya"]
+// console.log(myarr);
+// console.log(myarr[5]);
+// console.log(myarr.join());
+// let mynewarr=new Array(1,4,6,8)
+// console.log(mynewarr);
+
+
+//Array Methods
+
+// myarr.push("keshri")
+// console.log(myarr);
+// myarr.pop()
+// console.log(myarr);
+
+//shift and unshift
+let arr=[0,1,2,3,4,5]
+arr.unshift(9)//shift element to the staring of the array
+console.log(arr);
+// arr.shift();//shift to the left
+// console.log(arr);
+
+
+//slice and splice operator
+//the main difference between slice and splice is that 
+//   slice operator does not changes the original array while splice operator changes the original array
+
+// let slicedarr=arr.slice(0,3)
+// console.log(slicedarr);
+// console.log(arr);
+// let splicedarr=arr.splice(0,3)
+// console.log(splicedarr);
+// console.log(arr);
